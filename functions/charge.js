541941f7dca8bab81a5873a2e980c8425e66ca9a
hiro08gh <hiro08gh@gmail.com>
2019-12-21 15:15:13 +0900

    fix

--- functions/charge.js
+++ functions/charge.js
@@ -1,41 +1,38 @@
 require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const statusCode = 200;
 
 const headers = {
   'Access-Control-Allow-Origin': 'http://localhost:3000',
   'Access-Control-Allow-Headers': 'Content-Type',
 };
 
-exports.handler = function(event) {
+exports.handler = async function(event) {
   if (event.httpMethod !== 'POST') {
     return {
-      statusCode,
+      statusCode: 200,
       headers,
-      body: 'POSTリクエストではありません！',
+      body: 'Not POST Request',
     };
   }
 
   const data = JSON.parse(event.body);
 
   if (event.httpMethod === 'POST') {
-    stripe.charges
-      .create({
+    try {
+      await stripe.charges.create({
         amount: parseInt(data.amount),
         currency: 'jpy',
         description: 'An example charge',
         source: data.body,
-      })
-      .then(({status}) => {
-        return {
-          statusCode,
-          headers,
-          body: event.body,
-        };
-      })
-      .catch(err => {
-        console.log(err);
       });
+      return {
+        statusCode: 200,
+        headers,
+        body: event.body,
+      };
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
